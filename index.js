3294b0d396f243c8d848daf245a8f9f0e452b208
Jubilsmg <jubilj.d.s@gmail.com>
2024-09-30 20:43:35 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.getElementById('hamburger-menu');
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+hamburgerMenu.addEventListener('click', function () {
+        navbarNav.classList.toggle('active');
+    });
